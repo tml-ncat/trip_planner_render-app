99dc79bf8627215e81cf19ffc86ff959913b2f25
RidwanTiamiyu <ridtiamiyu@gmailcom>
2025-02-03 12:19:51 -0500

    docopt to build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "Updating and installing dependencies..."
 apt-get update && apt-get install -y openjdk-17-jdk
 
 # Ensure Python dependencies are installed
-pip install --no-cache-dir -r requirements.txt
+pip install docopt==0.6.2 --no-cache-dir && pip install -r requirements.txt
 
 # Set permissions for R5.jar if needed
 chmod +x ./src/R5.jar
